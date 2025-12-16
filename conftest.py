020d33313e4e5b2ba197feeaa96f9b86e3930ce3
tsubhashini23 <tsubhashini23@gmail.com>
2025-12-16 17:38:17 +0530

    Report config and parallel execution

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,3 +19,17 @@ def browser_setup(request):
         options=chr_options
     )
     request.cls.driver = driver
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    today = datetime.now()
+    report_dir = Path("hrm_reports", today.strftime("%Y%m%d"))
+    report_dir.mkdir(parents=True, exist_ok=True)
+    pytest_html = report_dir / f"Report_{today.strftime('%Y%m%d%H%M')}.html"
+    config.option.htmlpath = pytest_html
+    config.option.self_contained_html = True
+
+
+def pytest_html_report_title(report):
+    report.title = "HRM Test Report"
